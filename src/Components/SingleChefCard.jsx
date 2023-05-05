beba1e28b0f81dccc39a159e7dba959596cb0695
SarabanTahura1 <bristiakash01796@gmail.com>
2023-05-05 22:21:17 +0600

    add to favourite added

--- src/Components/SingleChefCard.jsx
+++ src/Components/SingleChefCard.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const SingleChefCard = ({ chef }) => {
+  const [isDisabled, setDisabled] = useState(false);
   const { id, picture, name, experience, num_recipes, likes } = chef;
+  const handleDisable = () => {
+    setDisabled(true);
+    Swal.fire({
+      title: "success",
+      text: "favourite added",
+      icon: "success",
+    });
+  };
+
   return (
     <div className="card w-full bg-base-100 shadow-xl">
       <figure className="px-10 pt-10">
@@ -19,13 +30,42 @@ const SingleChefCard = ({ chef }) => {
           <span>Likes : {likes}</span>
         </div>
         <span>No. of Recipe : {num_recipes}</span>
-        <div className=" text-center my-4">
+        <div className="my-4 flex justify-between items-center">
           <Link
             to={`/chef/${id}`}
             className="border-2 px-4 py-2  inline-block cursor-pointer rounded-md hover:border-green-500 text-green-600"
           >
             View Recipe
           </Link>
+
+          {isDisabled ? (
+            <button
+              onClick={handleDisable}
+              disabled
+              className="border-2 px-4 py-2  inline-block  rounded-md  text-gray-600"
+            >
+              Favourite
+            </button>
+          ) : (
+            <button
+              onClick={handleDisable}
+              className="border-2 px-4 py-2  inline-block cursor-pointer rounded-md border-green-500 text-green-600"
+            >
+              Favourite
+            </button>
+          )}
+
+          {/* 
+
+
+
+          <button
+            onClick={handleDisable}
+            disabled={isDisabled ? true : false}
+            className="border-2 px-4 py-2  inline-block cursor-pointer rounded-md hover:border-green-500 text-green-600"
+          >
+            Favourite
+          </button> */}
         </div>
       </div>
     </div>
